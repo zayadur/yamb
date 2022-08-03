ac0ca3fcafa8133946a3b18384090a517651e354
zayadur <zayadur@outlook.com>
2022-08-03 15:08:51 -0400

    Revert fix for CORS policy error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://yamb-alpha.vercel.app/:path*',
-      },
-    ]
-  },
 }
