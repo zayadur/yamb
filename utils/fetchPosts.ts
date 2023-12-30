52da3b4d0564c0d082b1d80e951857d0165b6cc4
zayadur <zaderkun@gmail.com>
2023-12-30 17:32:13 -0500

    Fix critical dev and main errors

--- utils/fetchPosts.ts
+++ utils/fetchPosts.ts
@@ -1,8 +1,12 @@
 import { Post } from '../typings'
-
 export const fetchPosts = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPosts`)
+  if (!res.ok) {
+    console.error(`Error fetching posts: ${res.status} ${res.statusText}`);
+    return [];
+  }
   const data = await res.json()
   const posts: Post[] = data.posts
   return posts
 }
+
